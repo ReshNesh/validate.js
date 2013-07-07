9c48ab28b2e0f4dfcc05e4e70744696117b3ebe9
Milan Misak <misakmilan@gmail.com>
2013-07-07 10:16:29 +0200

    A new rule for URLs with optional HTTP/HTTPS part

--- validate.js
+++ validate.js
@@ -36,7 +36,8 @@
             valid_base64: 'The %s field must contain a base64 string.',
             valid_credit_card: 'The %s field must contain a valid credit card number.',
             is_file_type: 'The %s field must contain only %s files.',
-            valid_url: 'The %s field must contain a valid URL.'
+            valid_url: 'The %s field must contain a valid URL.',
+            valid_url_without_http: 'The %s field must contain a valid URL.',
         },
         callback: function(errors) {
 
@@ -60,7 +61,8 @@
         ipRegex = /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})$/i,
         base64Regex = /[^a-zA-Z0-9\/\+=]/i,
         numericDashRegex = /^[\d\-\s]+$/,
-        urlRegex = /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
+        urlRegex = /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
+        urlWithoutHttpRegex = /^(((http|https):\/\/)?(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/;
 
     /*
      * The exposed public object to validate a form:
@@ -428,6 +430,10 @@
 
         valid_url: function(value, field) {
             return (urlRegex.test(value));
+        },
+
+        valid_url: function(value, field) {
+            return (urlWithoutHttpRegex.test(value));
         },
 
         valid_credit_card: function(value, field){
