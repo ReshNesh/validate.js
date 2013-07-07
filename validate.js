310717d7732a7f3ddc392c86000bdd7070073782
Milan Misak <misakmilan@gmail.com>
2013-07-07 10:31:01 +0200

    UK postcode regex is now case-insensitive

--- validate.js
+++ validate.js
@@ -66,7 +66,7 @@
         urlRegex = /^((http|https):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
         urlWithoutHttpRegex = /^(((http|https):\/\/)?(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
         ukPhoneNumber = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/,
-        ukPostcode = /^(GIR 0AA|[A-PR-UWYZ]([0-9][0-9A-HJKPS-UW]?|[A-HK-Y][0-9][0-9ABEHMNPRV-Y]?)[\s]{0,1}[0-9][ABD-HJLNP-UW-Z]{2})$/;
+        ukPostcode = /^(GIR 0AA|[A-PR-UWYZ]([0-9][0-9A-HJKPS-UW]?|[A-HK-Y][0-9][0-9ABEHMNPRV-Y]?)[\s]{0,1}[0-9][ABD-HJLNP-UW-Z]{2})$/i;
 
     /*
      * The exposed public object to validate a form:
